4a31a66c4746fa60f09283ff09ec31255d2f22c9
DnullP <Dnull123P@gmail.com>
2023-08-13 17:25:21 +0800

    Added the dependency of webpack

--- scripts/bundler.js
+++ scripts/bundler.js
@@ -2,14 +2,30 @@ const webpack = require('webpack');
 const path = require('path');
 
 hexo.on("generateBefore", function (post) {
+    var paths = hexo.theme.config.scripts
+    var source = hexo.theme.config.rawSource
+    var entry = paths.map(str => '../' + source + str);
+    var output = paths.map(str => 'public/' + str.replace(/\.js$/, '.js'));
+    var proceeding = []
+    console.log(__dirname)
+    entry.forEach((item, index) => {
+        proceeding.push({
+            entry: { path: path.resolve(__dirname, item) },
+            output: { filename: output[index], path: path.resolve('./') },
+            module: {
+                rules: [
+                    {
+                        test: /\.css$/i,
+                        use: ['style-loader', 'css-loader'],
+                    },
+                ],
+            },
+        })
+    })
     webpack(
-        {
-            entry: { path: path.resolve(__dirname, '../_source/js/test.js') },
-            output: { filename: 'target_test.js', path: path.resolve(__dirname, '../source/js') },
-        }
-        ,
+        proceeding,
         (err, stats) => {
             process.stdout.write(stats.toString() + '\n');
-        }
+        },
     );
 });
