55359f3a46ed2935b42e68f4f1b9fb8997b2fd2c
DnullP <Dnull123P@gmail.com>
2023-09-14 17:26:47 +0800

    recommend card effection

--- _source/js/csstool.js
+++ _source/js/csstool.js
@@ -4,18 +4,25 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomColor() {
+function getAlphaFromRGBA(rgbaString) {
+    // 解析rgba字符串并返回透明度 (alpha) 值
+    const match = rgbaString.match(/rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(\.\d+)?))?\)/);
+    return (match && match[4]) ? parseFloat(match[4]) : 1;
+}
+
+function getRandomColorWithAlpha(element) {
+    const originalAlpha = getAlphaFromRGBA(window.getComputedStyle(element).backgroundColor);
     const r = getRandomInt(200, 255);
     const g = getRandomInt(200, 255);
     const b = getRandomInt(200, 255);
-    return `rgb(${r}, ${g}, ${b})`;
+    return `rgba(${r}, ${g}, ${b}, ${originalAlpha})`;
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const elements = document.querySelectorAll('.random-color');
 
     elements.forEach((element) => {
-        const randomColor = getRandomColor();
+        const randomColor = getRandomColorWithAlpha(element);
         element.style.backgroundColor = randomColor;
     });
 });
